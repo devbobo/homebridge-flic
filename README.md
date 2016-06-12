10786c781bb6a47dc46f80899604e0237260fa50
David Parry <david@smugmug.com>
2016-06-12 14:06:23 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # homebridge-flic
+[![NPM Version](https://img.shields.io/npm/v/homebridge-flic.svg)](https://www.npmjs.com/package/homebridge-flic)
+
+[Flic](https://flic.io) plugin for [Homebridge](https://github.com/nfarina/homebridge).
+
+# Requirements
+
+This plugin requires the [Flic SDK for Linux](https://github.com/50ButtonsEach/fliclib-linux-hci) installed on a machine to run.
+
+# Installation
+
+1. Install homebridge using: npm install -g homebridge
+2. Install this plugin using: npm install -g homebridge-flic
+3. Update your configuration file. See the sample below.
+
+# Updating
+
+- npm update -g homebridge-lifx-lan
+
+# Configuration
+
+Configuration sample:
+
+ ```javascript
+"platforms": [
+    {
+        "platform": "Flic",
+        "name": "Flic",
+        "controllers": [
+            {"host": "locahost", "port": 5551}
+        ]
+    }
+]
+
+```
---
+++ index.js
@@ -0,0 +1,173 @@
+'use strict';
+
+var fliclib = require('./fliclib');
+var FlicClient = fliclib.FlicClient;
+var FlicConnectionChannel = fliclib.FlicConnectionChannel;
+
+var Accessory, Characteristic, Service, UUIDGen;
+
+module.exports = function (homebridge) {
+    Accessory = homebridge.platformAccessory;
+    Characteristic = homebridge.hap.Characteristic;
+    Service = homebridge.hap.Service;
+    UUIDGen = homebridge.hap.uuid;
+
+    homebridge.registerPlatform('homebridge-flic', 'Flic', FlicPlatform, true);
+};
+
+var Constants = {
+    DEFAULT_HOST: 'localhost',
+    DEFAULT_PORT: 5551,
+    SINGLE_CLICK: 1,
+    DOUBLE_CLICK: 2,
+    HOLD: 3
+}
+
+Constants.CLICK_TYPE = {
+    'ButtonSingleClick': Constants.SINGLE_CLICK,
+    'ButtonDoubleClick': Constants.DOUBLE_CLICK,
+    'ButtonHold':        Constants.HOLD
+}
+
+function FlicPlatform(log, config, api) {
+    var self = this;
+
+    this.config = config || {};
+    this.api = api;
+    this.accessories = {};
+    this.controllers = config.controllers || [{host: Constants.DEFAULT_HOST, port: Constants.DEFAULT_PORT}];
+    this.log = log;
+
+    this.api.on('didFinishLaunching', function() {
+        self.controllers.forEach(
+            function(controller) {
+                self.connectController(controller);
+            }
+        );
+    });
+}
+
+FlicPlatform.prototype.addAccessory = function(bdAddr) {
+    this.log("Found: %s", bdAddr);
+
+    var accessory = new Accessory(bdAddr, UUIDGen.generate(bdAddr));
+
+    accessory
+        .addService(Service.StatelessProgrammableSwitch)
+        .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
+        .setProps({ maxValue: 3 });
+
+    this.accessories[accessory.UUID] = accessory;
+    this.api.registerPlatformAccessories("homebridge-flic", "Flic", [accessory]);
+
+    return accessory;
+}
+
+FlicPlatform.prototype.configureAccessory = function(accessory) {
+    this.accessories[accessory.UUID] = accessory;
+}
+
+FlicPlatform.prototype.connectButton = function(client, bdAddr) {
+    var self = this;
+    var uuid = UUIDGen.generate(bdAddr);
+    var accessory = this.accessories[uuid];
+    var timeout;
+
+    if (accessory === undefined) {
+        accessory = this.addAccessory(bdAddr);
+    }
+
+    accessory.getService(Service.AccessoryInformation)
+        .setCharacteristic(Characteristic.Manufacturer, "Flic")
+        .setCharacteristic(Characteristic.Model, "Wireless Smart Button")
+        .setCharacteristic(Characteristic.SerialNumber, bdAddr);
+
+    accessory.updateReachability(true);
+
+    var cc = new FlicConnectionChannel(bdAddr);
+
+    client.addConnectionChannel(cc);
+
+    cc.on("buttonSingleOrDoubleClickOrHold", function(clickType, wasQueued, timeDiff) {
+        if (wasQueued == true && timeDiff > 5) {
+            return;
+        }
+
+        self.log("%s - %s", bdAddr, clickType);
+        accessory.getService(Service.StatelessProgrammableSwitch).getCharacteristic(Characteristic.ProgrammableSwitchEvent).setValue(Constants.CLICK_TYPE[clickType] || Constants.SINGLE_CLICK);
+
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            accessory.getService(Service.StatelessProgrammableSwitch).getCharacteristic(Characteristic.ProgrammableSwitchEvent).setValue(0);
+        }, 1000);
+    });
+
+    cc.on("connectionStatusChanged", function(connectionStatus, disconnectReason) {
+        self.log("%s - %s%s", bdAddr, connectionStatus, (connectionStatus == "Disconnected" ? " " + disconnectReason : ""));
+    });
+
+    cc.on("removed", function(reason) {
+        self.log("%s - Connection Removed (%s)", bdAddr, reason);
+        accessory.updateReachability(false);
+    });
+}
+
+FlicPlatform.prototype.connectController = function(controller) {
+    var self = this;
+
+    if (typeof controller !== 'object') {
+        controller = {host: Constants.DEFAULT_HOST, port: Constants.DEFAULT_PORT};
+    }
+
+    if (controller.host === undefined) {
+        controller.host = Constants.DEFAULT_HOST;
+    }
+
+    if (controller.port === undefined) {
+        controller.port = Constants.DEFAULT_PORT;
+    }
+
+    controller.buttons = [];
+    controller.client = new FlicClient(controller.host, controller.port);
+
+    controller.client.once('ready', function() {
+        self.log("Controller [%s:%s] - Connected", controller.host, controller.port);
+
+        controller.client.getInfo(function(info) {
+            info.bdAddrOfVerifiedButtons.forEach(function(bdAddr) {
+                controller.buttons.push(bdAddr);
+                self.connectButton(controller.client, bdAddr);
+            });
+        });
+    });
+
+    controller.client.on("bluetoothControllerStateChange", function(state) {
+        self.log("Controller [%s:%s] - %s", controller.host, controller.port, state);
+    });
+
+    controller.client.on("newVerifiedButton", function(bdAddr) {
+        controller.buttons.push(bdAddr);
+        self.connectButton(controller.client, bdAddr);
+    });
+
+    controller.client.on("error", function(error) {
+        self.log("Controller [%s:%s] - Error: %s", controller.host, controller.port, error);
+    });
+
+    controller.client.on("close", function(hadError) {
+        self.log("Controller [%s:%s] - Disconnected", controller.host, controller.port);
+
+        controller.buttons.forEach(
+            function(bdAddr) {
+                var uuid = UUIDGen.generate(bdAddr);
+                var accessory = self.accessories[uuid];
+
+                if (accessory !== undefined) {
+                    accessory.updateReachability(false);
+                }
+            }
+        );
+
+        setTimeout(function() {self.connectController(controller);}, 60000);
+    });
+}
